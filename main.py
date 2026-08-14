a58e47cbaeae586dd351b1487c4f8019f4782b92
swaroopughade <swaroopughade@gmail.com>
2026-08-14 12:22:35 +0530

    updated main.py to created tables automatically

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import models
+from database import engine
+
+# Automatically create all tables in PostgreSQL
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Student Academic Tracker")
 
 @app.get("/")
 def home():
-    return {"status": "success", "message": "Student Academic Tracker API is running"}
+    return {"status": "success", "message": "Database connected & API is running!"}
