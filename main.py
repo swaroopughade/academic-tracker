9e4880b765208ae7c3310a799c320056593aa07c
swaroopughade <swaroopughade@gmail.com>
2026-08-14 13:14:03 +0530

    updated main.py for first functional API endpoints for adding and fetching students

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy.orm import Session
 import models
-from database import engine
+import schemas
+from database import engine, get_db
 
-# Automatically create all tables in PostgreSQL
+# Ensure tables exist
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Student Academic Tracker")
@@ -10,3 +12,17 @@ app = FastAPI(title="Student Academic Tracker")
 @app.get("/")
 def home():
     return {"status": "success", "message": "Database connected & API is running!"}
+
+# --- STUDENT ENDPOINTS ---
+
+@app.post("/students/", response_model=schemas.StudentResponse, status_code=status.HTTP_201_CREATED)
+def create_student(student: schemas.StudentCreate, db: Session = Depends(get_db)):
+    db_student = models.Student(**student.model_dump())
+    db.add(db_student)
+    db.commit()
+    db.refresh(db_student)
+    return db_student
+
+@app.get("/students/", response_model=list[schemas.StudentResponse])
+def get_all_students(db: Session = Depends(get_db)):
+    return db.query(models.Student).all()
