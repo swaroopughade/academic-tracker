d9acdbd805450f597a805695ffee2ea0fdfd4a3c
swaroopughade <swaroopughade@gmail.com>
2026-08-14 12:40:19 +0530

    updated database.py to use 127.0.0.1 instead of localhost

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Updated with your verified PostgreSQL password: swaroop21
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:swaroop21@localhost:5432/academic_tracker_db"
+# Swapped 'localhost' to '127.0.0.1' to force IPv4 connection
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:swaroop21@127.0.0.1:5432/academic_tracker_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
